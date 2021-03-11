9d0c4af97481ee375f1a264fb3f8decff03c5e73
Nikolai <vova1988open@gmail.com>
2021-03-11 11:43:23 +0300

    feat: add 'towel sort' algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
+if(matrix === undefined){
   return [];
+}
+let res = [];
+for(let i = 0; i<matrix.length; i++){
+  if(i % 2 == 1){
+   matrix[i].reverse();
+  }
+  res = [].concat(...matrix);
+ 
+}
+return res;
 }
